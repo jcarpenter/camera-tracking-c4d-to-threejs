be222ed23eeaab753af6e76676bd3175fb74f87c
Josh Carpenter <jcarpenter@mozilla.com>
2016-09-04 10:45:27 -0700

    Got ae-to-json working

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-// var aeToJSON = require('ae-to-json/after-effects');
+var aeToJSON = require('ae-to-json/after-effects');
 var ae = require('after-effects');
-ae(() => alert("Hello!\nFrom node.js"));
+// ae(() => alert("Hello!\nFrom node.js"));
 
-// ae.execute(aeToJSON)
-// .then(function(json) {
-//   // do something with the json outout
-// })
-// .catch(function(e) {
-//   throw e;
-// });
+ae.execute(aeToJSON)
+.then(function(json) {
+  console.log(json);
+})
+.catch(function(e) {
+  throw e;
+});
